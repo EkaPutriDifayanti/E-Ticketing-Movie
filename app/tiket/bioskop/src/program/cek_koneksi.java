2c43dccece4df0c3c7f2f8479885e3681990bc47
Resalfin <resalfin88@gmail.com>
2023-11-26 20:42:55 +0800

    menambahkan package src dan bin , folder img, folder program dan menambahkan login page

---
+++ app/tiket/bioskop/src/program/cek_koneksi.java
@@ -0,0 +1,5 @@
+package app.tiket.bioskop.src.program;
+
+public class cek_koneksi {
+
+}
---
+++ app/tiket/bioskop/src/program/login/loginPage.java
@@ -0,0 +1,14 @@
+package app.tiket.bioskop.src.program.login;
+
+import javax.swing.*;
+
+public class loginPage {
+    private JTextField tfemail;
+    private JTextField tfpass;
+    private JButton logInButton;
+    private JPanel tfLoginpage;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
